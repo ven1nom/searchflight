fe51c6e2d5eb1d75b810dde078b541913f3081a5
ven1nom <your-email@example.com>
2024-12-21 01:35:56 +0530

    "Create models and migrated models , created first data entry using reporsitory"

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ const app=express();
 // console.log(process.env)
 const {PORT}=require('./config/serverConfig');
 const bodyParser = require('body-parser');
+const CityRepository = require('./repositories/city-repo');
+// const {City}=require('./models/index')
 
 const setupAndStartServer=async ()=>{
 // const PORT=3000;
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended:true}))
-  app.listen(PORT,()=>{
+  app.listen(PORT,async ()=>{
     console.log(`Server started at ${PORT}`)
+    // await City.create({
+    //   name:"New Delhi"
+      // this will create a row inside city table name: New Delhi
+    // })
+
+    //OR
+    const repo=new CityRepository();
+    repo.createCity({name:"New Delhi"})
 })
 }
 
